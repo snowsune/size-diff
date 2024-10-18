d8d54ac813e77c177c77839c9133346e7f25ab85
Snowsune <anonymousfyf@gmail.com>
2024-10-18 13:33:54 -0400

    Better with wsgi

--- app/utils/species_lookup.py
+++ app/utils/species_lookup.py
@@ -2,7 +2,7 @@ import yaml
 
 
 def load_species_data(species_name):
-    file_path = f"species_data/{species_name}.yaml"
+    file_path = f"app/species_data/{species_name}.yaml"
     with open(file_path, "r") as file:
         data = yaml.safe_load(file)
     return data
